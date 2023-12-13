992a569c00ad15750bd26e96a0b3c45d3304a4af
sarf01k <isaacsarfo2004@gmail.com>
2023-12-13 00:00:32 +0000

    refactor: booking

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -33,11 +33,28 @@ exports.getBooking = async (req, res) => {
     }
 }
 
+const timeStringToMinutes = (timeString) => {
+    const [time, ampm] = timeString.split(' ');
+    const [hours, minutes] = time.split(':').map(Number);
+
+    const adjustedHours = ampm === 'PM' && hours !== 12 ? hours + 12 : hours;
+
+    return adjustedHours * 60 + minutes;
+};
+
 exports.createBooking = async (req, res) => {
-    const { userId, serviceId, address, startDate, startTime, additionalInfo } = req.body;
-    const newBooking = new Booking({ userId, serviceId, address, startDate, startTime, additionalInfo });
+    const { userId, serviceId, address, startDate, startTime, duration, jobDescription, phoneNumber } = req.body;
+    const newBooking = new Booking({ userId, serviceId, address, startDate, startTime, duration, jobDescription, phoneNumber });
 
     try {
+        const totalMinutes = timeStringToMinutes(startTime) + parseInt(duration * 60);
+        if (totalMinutes > (22 * 60)) {
+            return res.status(400).json({
+                success: false,
+                message: "Work duration must not exceed 15 hours."
+            });
+        }
+
         const savedBooking = await newBooking.save();
 
         return res.status(200).json({
